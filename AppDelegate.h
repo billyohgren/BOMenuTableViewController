3019c4a589cc9d2427933f56ee991c211537d03e
billy <billy@beeimages.se>
2012-07-23 20:44:08 +0200

    Created a new Xcode project

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BOMenuTableViewController
+//
+//  Created by Billy Öhgren on 7/23/12.
+//  Copyright (c) 2012 Left wing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BOMenuTableViewController
+//
+//  Created by Billy Öhgren on 7/23/12.
+//  Copyright (c) 2012 Left wing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
